da72b776e154059e37dcd3afa9deca4dae588ed9
AlexandrePereira0011 <alexandrepereira0011@gmail.com>
2021-04-19 11:11:45 +0100

    Calculadora_Navbar_v1

--- AulaV2/src/app/components/calculator/calculator.component.ts
+++ AulaV2/src/app/components/calculator/calculator.component.ts
@@ -11,5 +11,35 @@ export class CalculatorComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  tela:any="";
+  conta:any="";
+  ultimoNumero:number;
 
+
+  mudarMostrador(event){
+    if (event.target.value=="C"){
+      this.tela="";
+      this.conta="";
+    }else if (event.target.value=="x"){
+      this.conta = this.conta + "*";
+      this.tela = this.conta;
+    }else if (event.target.value=="+/-"){
+      if (Math.sign(Number(this.conta))){
+        this.conta = "+" + this.conta ;
+        this.tela = this.conta;
+      }else if (this.ultimoNumero>0){
+        this.conta = this.conta;
+        this.tela = this.conta;
+      }
+    }else{
+        this.conta = this.conta + event.target.value;
+        this.tela = this.conta;
+    }
+  }
+
+  calcular(){
+    this.conta = eval(this.conta);
+    this.tela = this.conta;
+  }
+ 
 }
